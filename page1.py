04750f324ac3891281f9ed762006618ec35c7575
David Li <103528452+MisterMan7035@users.noreply.github.com>
2022-06-07 18:42:58 +0000

    loading screen

--- page1.py
+++ page1.py
@@ -1,12 +1,17 @@
 #Use of this page is optional. If you use code here, make sure either import page1 or from page1 import * appear on your main.py page.'
 import time
 import os
+import random
 
-x = 1
 dotArray = "Loading"
-print("Loading")
-while x < 100:
-  time.sleep(0.5)
+x = 1
+y = random.randint(30, 50)
+while x < y:
+  time.sleep(0.1)
+  os.system("clear")
   dotArray = dotArray + '.'
   print(dotArray)
-  os.clear
+  x = x + 1
+
+time.sleep(10)
+print(x)
