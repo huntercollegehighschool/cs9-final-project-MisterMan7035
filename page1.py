b82a2d0f5ce027e2442390acd3a8e45c638eebfe
David Li <103528452+MisterMan7035@users.noreply.github.com>
2022-06-07 19:17:39 +0000

    Basic enemy generator made

--- page1.py
+++ page1.py
@@ -5,13 +5,43 @@ import random
 
 dotArray = "Loading"
 x = 1
-y = random.randint(30, 50)
+t = 0.001
+y = random.randint(8, 12)
 while x < y:
-  time.sleep(0.1)
+  time.sleep(t)
+  t = t * 2
   os.system("clear")
   dotArray = dotArray + '.'
   print(dotArray)
   x = x + 1
 
-time.sleep(10)
-print(x)
+time.sleep(3)
+os.system("clear")
+time.sleep(3)
+
+def enemyspawn(enemyname, enemyhealth, enemydamage, enemyclass):
+  approachdeterm = random.randint(1, 3)
+  approachtext = 0
+  if approachdeterm == 1: 
+    approachtext = " challenges you!"
+  if approachdeterm == 2:
+    approachtext = " approaches you!"
+  if approachdeterm == 3: 
+    approachtext = " encounters you!"
+  #Randomly selects an approach text
+  
+  ehealth1 = enemyhealth + random.randint(1, 5)
+  #Used to randomly scale enemy health up or down 5
+
+  determ = random.randint(1, 2)
+  if determ == 1: 
+    enemyhealth = enemyhealth + ehealth1
+  if determ == 2:
+    enemyhealth = enemyhealth - ehealth1
+  #Determines whether to increase enemyhealth by 1-5 or decrease by 1-5
+    
+  result = str(enemyname) + str(approachtext) + "\nTheir health is: " + str(enemyhealth) + "\nTheir class is: " + str(enemyclass)
+  return result
+
+
+print(enemyspawn("Combatant", 100, 10, "Mage"))
