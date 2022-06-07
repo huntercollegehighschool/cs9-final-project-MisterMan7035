69acac89368d3a61d14df9afe0d1e430e6a07751
David Li <103528452+MisterMan7035@users.noreply.github.com>
2022-06-07 19:29:29 +0000

    did damage in enemy spawn

--- page1.py
+++ page1.py
@@ -30,7 +30,7 @@ UNCOMMENT THIS SOMETIME!
 
 
 def enemyspawn(enemyname, enemyhealth, enemydamage, enemyclass):
-  approachdeterm = random.randint(1, 3)
+  approachdeterm = int(random.randint(1, 3))
   approachtext = 0
   if approachdeterm == 1: 
     approachtext = " challenges you!"
@@ -42,7 +42,7 @@ def enemyspawn(enemyname, enemyhealth, enemydamage, enemyclass):
   ehp1 = int(enemyhealth / 6)
   ehp2 = int(enemyhealth / 4)
   ehealth1 = random.randint(ehp1 , ehp2)
-  #Used to randomly scale enemy health up or down 5
+  #Used to randomly scale enemy health up or down by 1/4 - 1/6 of its health
 
   determ = random.randint(1, 2)
   if determ == 1: 
@@ -50,6 +50,16 @@ def enemyspawn(enemyname, enemyhealth, enemydamage, enemyclass):
   if determ == 2:
     enemyhealth = enemyhealth - ehealth1
   #Determines whether to increase enemyhealth by 1-5 or decrease by 1-5
+  edmg1 = int(enemydamage / 6)
+  edmg2 = int(enemydamage / 4)
+  edamage1 = random.randint(edmg1 , dmg2)
+  
+  determ = random.randint(1, 2)
+  if determ == 1: 
+    enemydamage = enemydamage + edamage1
+  if determ == 2:
+    enemydamage = enemydamage - edamage1
+
     
   result = str(enemyname) + str(approachtext) + "\nTheir health is: " + str(enemyhealth) + "\nTheir class is: " + str(enemyclass)
   return result
